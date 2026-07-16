6e82240a573e8c7d2f87a36a603f9c6038bebb47
tanusree irri <tanusreeirri@gmail.com>
2026-07-16 18:32:36 +0530

    Add solution for Sum of GCD of Formed Pairs

--- LeetCode/Sum-of-GCD-of-Formed-Pairs/Solution.cpp
+++ LeetCode/Sum-of-GCD-of-Formed-Pairs/Solution.cpp
@@ -1,21 +1,8 @@
-1class Solution {
-2public:
-3    long long gcdSum(vector<int>& nums) {
-4       vector<int>prefixgcd; 
-5       int currmax=0;
-6       for(int i=0;i<nums.size();i++){
-7        currmax=max(nums[i],currmax);
-8        prefixgcd.push_back(gcd(nums[i],currmax));
-9       }
-10       sort(prefixgcd.begin(),prefixgcd.end());
-11       int lp=0;
-12       int rp=nums.size()-1;
-13       long long ans=0;
-14       while(lp<rp){
-15        ans+=gcd(prefixgcd[lp],prefixgcd[rp]);
-16        lp++;
-17        rp--;
-18       }
-19    return ans;    
-20    }
-21};
+        mx = []
+        prefixMax = -inf
+
+        for x in nums:
+            prefixMax = max(prefixMax, x)
+            mx.append(prefixMax)
+
+        prefixGcd = [gcd(x, y) for x, y in zip(nums, mx)]
