a9bf6943fc22d4936d9552a4fe3f9f6047d9a7b7
tanusree irri <tanusreeirri@gmail.com>
2026-07-19 16:01:11 +0530

    Add solution for Find Greatest Common Divisor of Array

--- LeetCode/Find-Greatest-Common-Divisor-of-Array/Solution.cpp
+++ LeetCode/Find-Greatest-Common-Divisor-of-Array/Solution.cpp
@@ -1,8 +1,17 @@
 1class Solution {
 2public:
 3    int findGCD(vector<int>& nums) {
-4       sort(nums.begin(),nums.end());
-5       int n=nums.size()-1;
-6       return gcd(nums[0],nums[n]); 
-7    }
-8};
+4       int n=nums.size()-1;
+5       int max=nums[0];
+6       int min=nums[0];
+7       for(int i=0;i<=n;i++){
+8        if(nums[i]>max){
+9            max=nums[i];
+10        }
+11        if(nums[i]<min){
+12            min=nums[i];
+13        }
+14       }
+15       return gcd(min,max); 
+16    }
+17};
