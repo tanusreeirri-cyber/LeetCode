be0254caea6d4f397be49704e448d315469a77c8
tanusree irri <tanusreeirri@gmail.com>
2026-07-22 06:18:23 +0530

    Time: 190 ms (47.37%) | Memory: 12.6 MB (91.80%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -6,9 +6,9 @@ public:
         for(int i=2;i<n;i++){
             if(is_prime[i]){
                 count++;
-            }
-            for(int j=i*2;j<n;j+=i){
+            for(long long j=1LL*i*i;j<n;j+=i){
                 is_prime[j]=false;
+            }
             }
         }
         return count;
