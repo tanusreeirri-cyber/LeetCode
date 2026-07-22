b240169ef094dc968cfd6a10ee2a01fa58ef62ce
tanusree irri <tanusreeirri@gmail.com>
2026-07-22 06:12:08 +0530

    Time: 822 ms (5.01%) | Memory: 12.7 MB (65.03%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>is_prime(n+1,true);
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(is_prime[i]){
+                count++;
+            }
+            for(int j=i*2;j<n;j+=i){
+                is_prime[j]=false;
+            }
+        }
+        return count;
+    }
+};
