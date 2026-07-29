4be100eeaa2daf108c73ef295015b2586af94c3a
tanusree irri <tanusreeirri@gmail.com>
2026-07-29 07:12:55 +0530

    Add solution for Matrix Diagonal Sum

--- LeetCode/Matrix-Diagonal-Sum/Solution.cpp
+++ LeetCode/Matrix-Diagonal-Sum/Solution.cpp
@@ -5,7 +5,7 @@
 5       int sum=0;
 6       for(int i=0;i<n;i++){
 7            sum+=mat[i][i];
-8            if(i!=n-i-1){
+8            if(i!=n-i-1){//checking for common element to ignore adding it to sum twice
 9                sum+=mat[i][n-i-1];
 10            }
 11       }
