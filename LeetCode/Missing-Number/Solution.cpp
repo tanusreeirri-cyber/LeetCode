7bde16f802cdacc74f0b58989cda9a5ca9ddfdfa
tanusree irri <tanusreeirri@gmail.com>
2026-08-05 06:31:30 +0530

    Add solution for Missing Number

--- LeetCode/Missing-Number/Solution.cpp
+++ LeetCode/Missing-Number/Solution.cpp
@@ -1,12 +1,11 @@
 1class Solution {
 2public:
 3    int missingNumber(vector<int>& nums) {
-4        int n=nums.size();
-5       int sum=(n*(n+1))/2;
-6       int s2=0;
-7       for(int vals:nums){
-8        s2+=vals;
-9       }
-10       return sum-s2;
-11    }
-12};
+4    int ans=nums.size();
+5    for(int i=0;i<nums.size();i++){
+6        ans^=i;
+7        ans^=nums[i];
+8    }
+9    return ans;
+10    }
+11};
