109af9c8982006217b98493a4be3dcae6c58d667
tanusree irri <tanusreeirri@gmail.com>
2026-08-16 21:21:11 +0530

    Time: 12 ms (12.18%) | Memory: 131.3 MB (33.50%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0};
+
+        for (int x : stones)
+            cnt[x % 3]++;
+
+        if (cnt[1] == 0 && cnt[2] == 0)
+            return false;
+
+        if (cnt[0] % 2 == 0)
+            return cnt[1] >= 1 && cnt[2] >= 1;
+
+        return abs(cnt[1] - cnt[2]) > 2;
+    }
+};
