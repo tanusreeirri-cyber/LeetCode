c1218906a4066d26d28f01f86548796733bf954b
tanusree irri <tanusreeirri@gmail.com>
2026-08-25 18:38:37 +0530

    Time: 5 ms (15.61%) | Memory: 25.1 MB (24.36%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -3,8 +3,8 @@ public:
     int missingMultiple(vector<int>& nums, int k) {
     unordered_set<int>s(nums.begin(),nums.end());
     int multiple=k;
-    while(s.find(multiple)!=s.end()){
-        multiple=multiple+k;
+    while(s.count(multiple)){
+        multiple+=k;
     }
     return multiple;
     }
